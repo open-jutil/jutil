fb36015c85ca2882f7d06b11428ef6583c8568d5
Joern Ungermann (IEK-7 FZ-Juelich) <j.ungermann@fz-juelich.de>
2014-10-25 21:13:55 +0200

    updated testcase to actually work.

--- jutil/linalg.py
+++ jutil/linalg.py
@@ -9,22 +9,22 @@ def quick_diagonal_product(matrix, diagonal=None):
     if diagonal is None:
         diagonal = np.ones(matrix.shape[0])
     if type(matrix) is sp.csr_matrix:
-        result = np.zeros(x.shape)
+        result = np.zeros(diagonal.shape)
         for row_idx in xrange(matrix.shape[0]):
             row = matrix.getrow(row_idx)
             data = diagonal[row_idx] * (row.data ** 2)
             result[row.indices] += data
         return result
     elif type(matrix) is sp.csc_matrix:
-        result = np.zeros(x.shape)
-        for col_idx in xrange(matrix.shape[1]):   
+        result = np.zeros(diagonal.shape)
+        for col_idx in xrange(matrix.shape[1]):
             col = matrix.getcol(col_idx)
             result[col_idx] = np.dot(diagonal[col.indices], col.data ** 2)
         return result
     else:
-        result = np.zeros(x.shape)
-        for col_idx in xrange(matrix.shape[1]):   
+        result = np.zeros(diagonal.shape)
+        for col_idx in xrange(matrix.shape[1]):
             result[col_idx] = np.dot(diagonal, matrix[:, col_idx] ** 2)
         return result
- 
+
 
--- jutil/tests/test_linalg.py
+++ jutil/tests/test_linalg.py
@@ -1,29 +1,39 @@
 import jutil.linalg
-import scipy.sparse
+import scipy.sparse as sp
 import numpy as np
 from numpy.testing import assert_almost_equal
 
 
-def execute_matrix(A, diag):
-    ATA = A.T.dot(A)
+def execute_matrix(A, diag=None):
+    if diag is not None:
+        B = A
+        if hasattr(B, "todense"):
+            B = np.asarray(B.todense())
+        ATA = B.T.dot(np.diag(diag).dot(B))
+    else:
+        ATA = A.T.dot(A)
     assert_almost_equal(
-            np.diag(ATA),
+            ATA.diagonal().squeeze(),
             jutil.linalg.quick_diagonal_product(A, diag))
 
 
 def test_sparse_csr():
     A = sp.csr_matrix(np.arange(25).reshape(5, 5) / 25.)
     execute_matrix(A)
-    execute_matrix(A, np.arange(5))
+    execute_matrix(A, np.arange(5.))
 
 
 def test_sparse_csc():
     A = sp.csc_matrix(np.arange(25).reshape(5, 5) / 25.)
     execute_matrix(A)
-    execute_matrix(A, np.arange(5))
+    execute_matrix(A, np.arange(5.))
 
 
 def test_array():
     A = np.arange(25).reshape(5, 5) / 25.
     execute_matrix(A)
-    execute_matrix(A, np.arange(5))
+    execute_matrix(A, np.arange(5.))
+
+if __name__ == '__main__':
+    from numpy import testing
+    testing.run_module_suite()
