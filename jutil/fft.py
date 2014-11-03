b3bf737d4495b01f679edbf93178857a57d96411
Joern Ungermann (IEK-7 FZ-Juelich) <j.ungermann@fz-juelich.de>
2014-11-03 23:34:00 +0100

    bug fixing and update of testcases for fft.

--- jutil/fft.py
+++ jutil/fft.py
@@ -1,17 +1,23 @@
 import numpy as np
 
 
-def fft(x):
+def _fft(x):
     n = len(x)
-    return np.asarray([(x * np.exp(-np.pi * 2j * np.arange(n) * m / n)).sum() for m in np.arange(n)])
+    result = np.ndarray(n, dtype=np.complex)
+    for m in np.arange(n):
+        result[m] = (x * np.exp(-np.pi * 2j * np.arange(n) * m / n)).sum()
+    return result
 
 
-def ifft(x):
+def _ifft(x):
     n = len(x)
-    return np.asarray([(x * np.exp(np.pi * 2j * np.arange(n) * m / n)).sum() for m in np.arange(n)]) / n
+    result = np.ndarray(n, dtype=np.complex)
+    for m in np.arange(n):
+        result[m] = (x * np.exp(np.pi * 2j * np.arange(n) * m / n)).sum()
+    return result / n
 
 
-def rfft(x):
+def _rfft(x):
     n = len(x)
     result = np.zeros(n / 2 + 1, dtype=np.complex)
     for m in range(len(result)):
@@ -19,7 +25,7 @@ def rfft(x):
     return result
 
 
-def irfft(x, n=None):
+def _irfft(x, n=None):
     if n is None:
         n = 2 * (len(x) - 1)
     xp = np.ndarray(n, dtype=np.complex)
@@ -28,68 +34,74 @@ def irfft(x, n=None):
 
     extra = n % 2
     if extra == 0:
-        extra2 = -1
+        extra2 = -2
         xp[len(x) - 1] = xp[len(x) - 1].real
     else:
         extra2 = None
     xp[0] = x[0].real
 
-    xp[len(x):] = np.conj(x[1:extra2][::-1])
+    xp[len(x):] = np.conj(x[extra2:0:-1])
+
     result = np.zeros(n, dtype=np.complex)
     for m in np.arange(n):
-        result[m] = (xp * np.exp(np.pi * 2j * np.arange(n) * m / n)).sum() / n
-    return result
+        result[m] = (xp * np.exp(np.pi * 2j * np.arange(n) * m / n)).sum()
+    return result / n
 
 
-def rfftn(x):
+def _rfftn(x):
     return np.fft.fft(np.fft.rfft(x, axis=1), axis=0)
 
 
-def irfftn(x, n=None):
+def _irfftn(x, n=None):
     if n is None:
         n = 2 * (x.shape[1] - 1)
     return np.fft.irfft(np.fft.ifft(x, axis=0), n=n, axis=1)
 
 
+def fft_adj(x):
+    n = len(x)
+    return np.fft.ifft(x) * n
+
+
+def ifft_adj(x):
+    n = len(x)
+    return np.fft.fft(x) / n
+
+
 def rfft_adj(x, n=None):
     if n is None:
         n = 2 * (len(x) - 1)
-    xp = np.zeros(n, dtype=x.dtype)
-    xp[:len(x)] = x
-    return np.fft.fft(xp)
+    return np.fft.ifft(x, n=n).real * n
 
 
 def irfft_adj(x):
     n_out = len(x) / 2 + 1
-    xp = np.fft.ifft(x)
+    xp = np.fft.fft(x) / len(x)
     if len(x) % 2 == 0:
-        xp[1:n_out - 1] = np.conj(xp[1:n_out - 1]) + xp[:n_out - 1:-1]
-        xp[n_out - 1] = xp[n_out - 1].real
+        xp[1:n_out - 1] += np.conj(xp[:n_out - 1:-1])
+#        xp[n_out - 1] = xp[n_out - 1].real
     else:
-        xp[1:n_out] = np.conj(xp[1:n_out]) + (xp[:n_out - 1:-1])
-    xp[0] = xp[0].real
+        xp[1:n_out] += np.conj(xp[:n_out - 1:-1])
+#    xp[0] = xp[0].real
     return xp[:n_out]
 
 
 def rfftn_adj(x, n=None):
     if n is None:
         n = 2 * (x.shape[1] - 1)
-    shape = (x.shape[0], n)
-    xp = np.zeros(shape, dtype=x.dtype)
+    xp = np.zeros((x.shape[0], n), dtype=x.dtype)
     xp[:, :x.shape[1]] = x
-    return np.fft.fftn(xp)
+    return np.fft.ifftn(xp).real * x.shape[0] * n
 
 
 def irfftn_adj(x):
     n_out = x.shape[1] / 2 + 1
-    xp = np.fft.ifft(x, axis=1)
+    xp = np.fft.fft(x, axis=1) / x.shape[1]
     if x.shape[1] % 2 == 0:
-        xp[:, 1:n_out - 1] = np.conj(xp[:, 1:n_out - 1]) + xp[:, :n_out-1:-1]
-        xp[:, n_out - 1] = xp[:, n_out - 1].real
+        xp[:, 1:n_out - 1] += np.conj(xp[:, :n_out-1:-1])
+#        xp[:, n_out - 1] = xp[:, n_out - 1].real
     else:
-        xp[:, 1:n_out] = np.conj(xp[:, 1:n_out]) + (xp[:, :n_out-1:-1])
-        xp[:, 0] = xp[:, 0].real
-    xp = xp[:, :n_out]
-    #xp2 = np.asarray([irfft_adj(x[i, :]) for i in range(x.shape[0])])
-    return np.fft.fft(xp, axis=0) / xp.shape[0]
+        xp[:, 1:n_out] += np.conj(xp[:, :n_out-1:-1])
+#    xp[:, 0] = xp[:, 0].real
+    return np.fft.fft(xp[:, :n_out], axis=0) / xp.shape[0]
 
--- jutil/tests/test_fft.py
+++ jutil/tests/test_fft.py
@@ -1,13 +1,14 @@
 import numpy as np
-import jutil.fft as jfft
 from numpy.testing import assert_equal, assert_almost_equal
 
+import jutil.fft as jfft
 import jutil.diff as jdiff
 
 NS = [2, 3, 4, 5, 10, 11, 16, 17, 21]
 
 
 def r2c(x):
+    print x.shape
     r, i = np.split(x, 2)
     return r + 1j * i
 
@@ -17,74 +18,105 @@ def c2r(x):
     return np.concatenate([r, i])
 
 
+def wrap(f, shape=None, ci=False, co=False, kw={}):
+    def result(x):
+        if ci:
+            x = r2c(x)
+        if shape is not None:
+            x = x.reshape(shape)
+        y = f(x, **kw).reshape(-1)
+        if co:
+            y = c2r(y)
+        return y
+    return result
+
+
 def test_fft():
     for n in NS:
         x = np.random.random(n) + 1j * np.random.random(n)
-        assert_almost_equal(np.fft.fft(x), jfft.fft(x))
+        assert_almost_equal(np.fft.fft(x), jfft._fft(x))
 
 
 def test_ifft():
     for n in NS:
         x = np.random.random(n) + 1j * np.random.random(n)
-        assert_almost_equal(np.fft.ifft(x), jfft.ifft(x))
+        assert_almost_equal(np.fft.ifft(x), jfft._ifft(x))
 
 
 def test_rfft():
     for n in NS:
         x = np.random.random(n)
-        assert_almost_equal(np.fft.rfft(x), jfft.rfft(x))
+        assert_almost_equal(np.fft.rfft(x), jfft._rfft(x))
 
 
 def test_irfft():
     for n in NS:
         m = n / 2 + 1
         x = np.random.random(m) + 1j * np.random.random(m)
-        print x, np.fft.irfft(x, n=n)#, jfft.irfft(x, n=n)
-        assert_almost_equal(np.fft.irfft(x, n=n), jfft.irfft(x, n=n))
+        assert_almost_equal(np.fft.irfft(x, n=n), jfft._irfft(x, n=n))
 
 
 def test_rfftn():
     for n1 in NS:
         for n2 in NS:
             x = np.random.random((n1, n2))
-            assert_almost_equal(np.fft.rfftn(x), jfft.rfftn(x))
+            assert_almost_equal(np.fft.rfftn(x), jfft._rfftn(x))
 
 def test_irfftn():
     for n1 in NS:
         for n2 in NS:
             m = n2 / 2 + 1
             x = np.random.random((n1, m)) + 1j * np.random.random((n1, m))
-            assert_almost_equal(np.fft.irfftn(x, s=(n1, n2)), jfft.irfftn(x, n=n2))
+            assert_almost_equal(np.fft.irfftn(x, s=(n1, n2)), jfft._irfftn(x, n=n2))
+
+
+def test_fft_adj():
+    for n in NS:
+        A = jdiff.fd_jac(wrap(np.fft.fft, None, True, True), np.zeros(2 * n))
+        B = jdiff.fd_jac(wrap(jfft.fft_adj, None, True, True), np.zeros(2 * n)).T
+        assert_almost_equal(abs(A - B).sum(), 0, err_msg=str((A, B)))
+
+        x = np.random.random(2 * n)
+        ATx_1 = A.T.dot(x)
+        ATx_2 = c2r(jfft.fft_adj(r2c(x)))
+        assert_almost_equal(ATx_1, ATx_2)
+
+
+def test_ifft_adj():
+    for n in NS:
+        A = jdiff.fd_jac(wrap(np.fft.ifft, None, True, True), np.zeros(2 * n))
+        B = jdiff.fd_jac(wrap(jfft.ifft_adj, None, True, True), np.zeros(2 * n)).T
+        assert_almost_equal(abs(A - B).sum(), 0, err_msg=str((A, B)))
+
+        x = np.random.random(2 * n)
+        ATx_1 = A.T.dot(x)
+        ATx_2 = c2r(jfft.ifft_adj(r2c(x)))
+        assert_almost_equal(ATx_1, ATx_2)
 
 
 def test_rfft_adj():
     for n in NS:
-        A = jdiff.fd_jac(np.fft.rfft, np.zeros(n))
-        x = np.random.random(A.shape[0]) + 1j * np.random.random(A.shape[0])
+        m = n / 2 + 1
+
+        A = jdiff.fd_jac(wrap(np.fft.rfft, None, False, True), np.zeros(n))
+        B = jdiff.fd_jac(wrap(jfft.rfft_adj, None, True, False, {"n":n}), np.zeros(2 * m)).T
+        assert_almost_equal(abs(A - B).sum(), 0, err_msg=str((A,B)))
+
+        x = np.random.random(A.shape[0])
         ATx_1 = A.T.dot(x)
-        ATx_2 = jfft.rfft_adj(x, n=n)
-        print A.T.shape, ATx_1.shape, ATx_2.shape, x.shape, n
-        print ATx_1 - ATx_2
+        ATx_2 = jfft.rfft_adj(r2c(x), n=n)
         assert_almost_equal(ATx_1, ATx_2)
 
 
 def test_irfft_adj():
     for n in NS:
         m = n / 2 + 1
-        def irfft_wrap(x):
-            x = r2c(x)
-            result = np.fft.irfft(x.reshape(m), n=n)
-            print "pp", x, result.reshape(-1)
-            return result.reshape(-1)
-        A = jdiff.fd_jac(irfft_wrap, np.zeros(m * 2))
 
-        def irfft_wrap2(x):
-            result = jfft.irfft_adj(x)
-            return c2r(result.reshape(-1))
-        B = jdiff.fd_jac(irfft_wrap2, np.zeros(n)).T
-        assert_almost_equal(abs(A - B).sum(), 0)
+        A = jdiff.fd_jac(wrap(np.fft.irfft, m, True, False, {"n":n}), np.zeros(m * 2))
+        B = jdiff.fd_jac(wrap(jfft.irfft_adj, None, False, True), np.zeros(n)).T
+        assert_almost_equal(abs(A - B).sum(), 0, err_msg=str((A, B)))
 
-        x = np.random.random(A.shape[0])# + 1j * np.random.random(A.shape[0])
+        x = np.random.random(A.shape[0])
         ATx_1 = r2c(A.T.dot(x))
         ATx_2 = jfft.irfft_adj(x)
         assert_almost_equal(ATx_1, ATx_2)
@@ -93,96 +125,32 @@ def test_irfft_adj():
 def test_rfftn_adj():
     for n1 in NS:
         for n2 in NS:
-            def rfftn_wrap(x):
-                result = np.fft.rfftn(x.reshape(n1, n2))
-                return result.reshape(-1)
-            A = jdiff.fd_jac(rfftn_wrap, np.zeros(n1 * n2))
+            m = n2 / 2 + 1
 
-            x = np.random.random(A.shape[0]) + 1j * np.random.random(A.shape[0])
+            A = jdiff.fd_jac(wrap(np.fft.rfftn, (n1, n2), False, True), np.zeros(n1 * n2))
+            B = jdiff.fd_jac(wrap(jfft.rfftn_adj, (n1, m), True, False, {"n":n2}),
+                             np.zeros(2 * n1 * m)).T
+            assert_almost_equal(abs(A - B).sum(), 0, err_msg=str((A, B)))
+
+            x = np.random.random(A.shape[0])
             ATx_1 = A.T.dot(x)
-            ATx_2 = jfft.rfftn_adj(x.reshape(n1, n2 / 2 + 1), n=n2).reshape(-1)
-            assert_almost_equal(ATx_1, ATx_2)
+            ATx_2 = jfft.rfftn_adj(r2c(x).reshape(n1, m), n=n2).reshape(-1)
+            assert_almost_equal(ATx_2, ATx_2, err_msg=str((ATx_1, ATx_2)))
 
 
 def test_irfftn_adj():
     for n1 in [2, 4, 10, 11, 16, 17, 21]:
         for n2 in [2, 4, 10, 11, 16, 17, 21]:
-            print
-            print "==========================================="
-            print n1, n2
             m = n2 / 2 + 1
 
-            def irfftn_wrap(x):
-                x = r2c(x)
-                result = np.fft.irfftn(x.reshape(n1, m), s=(n1, n2))
-                return result.reshape(-1)
-            A = jdiff.fd_jac(irfftn_wrap, np.zeros(n1 * m * 2))
-            """
-            def irfftn_wrap2(x):
-                result = jfft.irfftn_adj(x.reshape(n1, n2))
-                return c2r(result.reshape(-1))
-            B = jdiff.fd_jac(irfftn_wrap2, np.zeros(n1 * n2)).T
+            A = jdiff.fd_jac(wrap(np.fft.irfftn, (n1, m), True, False, {"s":(n1, n2)}),
+                             np.zeros(n1 * m * 2))
+            B = jdiff.fd_jac(wrap(jfft.irfftn_adj, (n1, n2), False, True), np.zeros(n1 * n2)).T
+            assert_almost_equal(abs(A - B).sum(), 0, err_msg=str((A, B)))
 
-            def ifft_wrap(x):
-                x = r2c(x)
-                result = np.fft.ifft(x.reshape(n1, m), axis=0)
-                return c2r(result.reshape(-1))
-            C = jdiff.fd_jac(ifft_wrap, np.zeros(n1 * m * 2))
-
-            def irfft_wrap(x):
-                x = r2c(x)
-                result = np.fft.irfft(x.reshape(n1, m), n=n2, axis=1)
-                return result.reshape(-1)
-            D = jdiff.fd_jac(irfft_wrap, np.zeros(n1 * m * 2))
-
-            def irfft_wrap2(x):
-                x = x.reshape(n1, n2)
-                result = np.asarray([jfft.irfft_adj(x[i, :]) for i in range(x.shape[0])])
-                return c2r(result.reshape(-1))
-            D2 = jdiff.fd_jac(irfft_wrap2, np.zeros(n1 * n2)).T
-
-            def dummy_wrap(x):
-                x = irfft_wrap(x)
-                print x.shape
-                x = ifft_wrap(x)
-                return x
-#            DU = jdiff.fd_jac(dummy_wrap, np.zeros(n1 * m * 2))
-
-
-           #C = C.reshape(n1*n2,n1,m)
-            #C[:,:,0] = C[:,:,0].real
-            #if n2 % 2 == 0:
-            #    C[:,:,-1] = C[:,:,-1].real
-
-#            C[:,:,2] = C[:,:,2].real
-            #C=C.reshape(n1*n2, n1*m)
-
-            print "A", A
-            print "B", B
-            print "C", C, C.shape
-            print "DC", D.dot(C)
-#            print "DU", DU
-            print "D", D
-            print "D2", D2
-            print "C-C.T", abs(C-C.T).sum()
-
-            print A.shape, B.shape, "\nA-B", abs(A-B).sum()
-            print "A-D", abs(A - D.dot(C)).sum()
-            print "D2-D", abs(D - D2).sum()
-            x = np.random.random(A.shape[1])# + 1j * np.random.random(A.shape[1])
-            print "A", A.dot(x).reshape(n1, n2)
-            print "B", B.dot(x).reshape(n1, n2)
-#            print "C", np.fft.irfftn(x.reshape(n1, m), s=(n1,n2))
-            """
-            x = np.random.random(A.shape[0])# + 1j * np.random.random(A.shape[0])
+            x = np.random.random(A.shape[0])
             ATx_1 = r2c(A.T.dot(x))
             ATx_2 = jfft.irfftn_adj(x.reshape(n1, n2)).reshape(-1)
-            #print ATx_1.shape, ATx_2.shape
-            #print "1\n", ATx_1
-            #print "2\n", ATx_2
-            #print A.shape
-            ATx_2 = ATx_2.reshape(-1)
-            #print abs(ATx_1 - ATx_2).sum()
             assert_almost_equal(ATx_1, ATx_2)
 
 
